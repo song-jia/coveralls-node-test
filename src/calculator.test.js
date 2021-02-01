4e781aad0336ff506375deef50107bf845ca797a
Song Jia <songjiacn@outlook.com>
2021-02-01 11:50:57 +0800

    add unit test of sub.

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,9 +1,18 @@
 const cal = require('./calculator');
 const assert = require('assert');
 
-describe('add', () => {
-    it('should add a and b', () => {
-        const res = cal.add(1, 2);
-        assert.strictEqual(res, 3);
+describe('calculator', () => {
+    describe('add', () => {
+        it('should add a and b', () => {
+            const res = cal.add(1, 2);
+            assert.strictEqual(res, 3);
+        });
+    });
+
+    describe('sub', () => {
+        it('should a sub b', () => {
+            const res = cal.sub(2, 1);
+            assert.strictEqual(res, 1);
+        });
     });
 });
